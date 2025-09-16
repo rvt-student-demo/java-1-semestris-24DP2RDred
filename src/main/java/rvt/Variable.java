ea8d6fa56bf0672666fb8eeb7bc6d0abaa974bb7
Roberts Dredžels <A241969RD@rvt.lv>
2025-09-16 09:54:33 +0300

    Variable

---
+++ src/main/java/rvt/Variable.java
@@ -0,0 +1,19 @@
+package rvt;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        int chicken = 9000;
+        double bacon = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chicken:" + "\n" + chicken);
+        System.out.println("Bacon (kg):" + "\n" + bacon);
+        System.out.println("Tractor:" + "\n" + tractor);
+
+        System.out.println("\n" + "And finally, a summary" + "\n" + chicken + "\n" + bacon + "\n" + tractor);
+    }
+
+    
+}
