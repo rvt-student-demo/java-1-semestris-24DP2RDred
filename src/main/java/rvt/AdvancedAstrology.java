e13ccac6f6b07d8941518f7f1a999edf3a1e5500
Roberts Dredžels <A241969RD@rvt.lv>
2025-10-07 12:35:14 +0300

    java mooc2-4

---
+++ src/main/java/rvt/AdvancedAstrology.java
@@ -0,0 +1,43 @@
+package rvt;
+
+public class AdvancedAstrology {
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(" "); 
+        }
+    }
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i); 
+            printStars(i);         
+        }
+    }
+    public static void christmasTree(int height) {
+        
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1); 
+        }
+
+        for (int i = 0; i < 2; i++) {
+            printSpaces(height - 2); 
+            printStars(3);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Triangle:");
+        AdvancedAstrology.printTriangle(4);
+
+        System.out.println("\nChristmas tree:");
+        AdvancedAstrology.christmasTree(10);
+    }
+}
+
---
+++ src/main/java/rvt/DivisibleByThree.java
@@ -0,0 +1,18 @@
+package rvt;
+
+public class DivisibleByThree {
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        divisibleByThreeInRange(3, 6);
+        System.out.println(); 
+        divisibleByThreeInRange(2, 10);
+    }
+}
